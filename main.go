603579054b77eeed1dd5a3e6536a5e2a4f72361f
bokwoon <bokwoon.c@gmail.com>
2019-05-15 02:45:36 +0800

    Refactored css into separate file
    But to do so I had to serve static assets using chi-router so that the
    css files could be found. (That adds more complexity, but is unavoidable
    since I would have to serve js files the same way as well.)
    
    Also added the skeleton if/else template logic for switching the navbar
    between public/participant/adviser/mentor/admin mode depending on what
    variables you pass to the template.
    
    TODO: properly flesh out the navbar for each mode (right now they are
    all identical). Also figure out the correct data struct to pass to the
    navbar template. The struct will provide some sort of guideline for what
    variables to pass to the templates, so it's not like you have to trace
    the entire template paths in order to figure it out at least.

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-chi/chi"
 )
@@ -12,17 +15,26 @@ func main() {
 
 	// About Orbital
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
 		mustExecute(
 			w,
 			mustParse(
 				"templates/aboutOrbital.html",
 				"templates/navbars.html",
 			),
-			nil,
+			map[string]string{
+				"role":                  "participant",
+				"display_name":          "User01",
+				"participantTeamStatus": "teamless",
+			},
 		)
 	})
 
+	// Ensure files in static/ are available to the public
+	workDir, _ := os.Getwd()
+	filesDir := filepath.Join(workDir, "static")
+	FileServer(r, "/static", http.Dir(filesDir))
+
+	// Start the server
 	http.ListenAndServe(":3000", r)
 }
 
@@ -38,4 +50,24 @@ func mustExecute(w http.ResponseWriter, t *template.Template, data interface{}) 
 	if err := t.Execute(w, data); err != nil {
 		panic(err)
 	}
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
 }
