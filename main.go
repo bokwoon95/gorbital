bf0efa1917adc76bc1efb2968825dcc372a0a071
bokwoon <bokwoon.c@gmail.com>
2019-05-14 19:24:32 +0800

    added MVP of embedding templates within templates
    Also flexbox is amazing, so much less code needed to create a navbar.
    Definitely what I would use, why bother with float: left and display: block
    black magic?
    
    I replicated DOM Shadowing in HTML/CSS by using a class hash in the
    wrapper DOM element. CSS can then by restricted to this specific DOM
    element by spcifying the class hash for every CSS rule.
    
    The hash is the last 7 characters of the SHA1 of the containing
    template's name. E.g. for the nav element inside navbars.html:
      <nav class="55a11c7"> ... </nav>
    The hash 55a11c7 was obtained by this shell one liner:
    echo "navbars.html" | sha1sum | awk '{printf "%s",$1}' | tail -c 7
    
    The CSS can then be restricted to this particular element like so:
    nav[class='55a11c7'] > ul > li > a {
        display: flex;
        padding: 8px 16px 8px 16px;
        color: #9BD5FD;
        text-decoration: none;
        font-family: Roboto Condensed, serif;
    }

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/go-chi/chi"
@@ -8,8 +9,33 @@ import (
 
 func main() {
 	r := chi.NewRouter()
+
+	// About Orbital
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		w.Header().Set("Content-Type", "text/html")
+		mustExecute(
+			w,
+			mustParse(
+				"templates/aboutOrbital.html",
+				"templates/navbars.html",
+			),
+			nil,
+		)
 	})
+
 	http.ListenAndServe(":3000", r)
+}
+
+func mustParse(templateFiles ...string) *template.Template {
+	t, err := template.ParseFiles(templateFiles...)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func mustExecute(w http.ResponseWriter, t *template.Template, data interface{}) {
+	if err := t.Execute(w, data); err != nil {
+		panic(err)
+	}
 }
